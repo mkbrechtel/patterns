e22c55b9c78e27110088c0f96f1ba48afd92fb4b
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2025-01-11 00:48:02 +0100

    Adjust markdown files to fit into starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Cute Patterns! 💠',
-			social: {
-				github: 'https://github.com/mkbrechtel/patterns',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://patterns.mkbrechtel.dev",
+  integrations: [
+    starlight({
+      title: "Cute Patterns! 💠",
+      social: {
+        github: "https://github.com/mkbrechtel/patterns",
+      },
+      sidebar: [
+        {
+          label: "Cute Patterns! 💠",
+          link: "/",
+        },
+        {
+          label: "Deployment",
+          autogenerate: { directory: "deployment" },
+        },
+      ],
+    }),
+  ],
 });
