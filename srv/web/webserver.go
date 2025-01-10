b95b297e7311114f7e0287ab28ff9ed0d7026c52
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2025-01-10 21:35:16 +0100

    listen to port 4780

--- srv/web/webserver.go
+++ srv/web/webserver.go
@@ -11,8 +11,8 @@ func main() {
     fileServer := http.FileServer(http.FS(content.ContentFiles))
     mux.Handle("/", fileServer)
 
-    fmt.Println("Starting server on :8080")
-    err := http.ListenAndServe(":8080", mux)
+    fmt.Println("Starting server on :4780")
+    err := http.ListenAndServe(":4780", mux)
     if err != nil {
         fmt.Printf("Server error: %v\n", err)
     }
